787c8e89fdc07e4825815c812864819cff2baddd
mrohmer <rohmer.matze@gmail.com>
2022-07-13 22:59:31 +0200

    fix: fix cartographer size

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -50,14 +50,14 @@
         <div class="text-center font-cinzel uppercase mt-4 mb-8">
             <div class="inline-block">
                 <div class="text-yellow-600">
-                    <div class="text-xl text-left pl-[5.4rem] -mb-[0.4rem]"
+                    <div class="text-xl text-left pl-[2.7rem] sm:pl-[5.4rem] -mb-[0.4rem]"
                          style="text-shadow: 1px  1px 1px black, 1px -1px 1px black, -1px  1px 1px black, -1px -1px 1px black;">{$_('game.article', {default: ''})}</div>
-                    <div class="text-6xl"
+                    <div class="text-3xl sm:text-6xl"
                          style="text-shadow: 1px  1px 2px black, 1px -1px 2px black, -1px  1px 2px black, -1px -1px 2px black;">{$_('game.name', {default: ''})}</div>
                 </div>
-                <div class="flex items-center -mt-[0.4rem]">
+                <div class="flex items-center sm:-mt-[0.4rem]">
                     <div class="flex-1 h-px bg-black"></div>
-                    <div class="mx-2">{$_('game.scorecard', {default: ''})}</div>
+                    <div class="mx-2 text-xs sm:text-normal">{$_('game.scorecard', {default: ''})}</div>
                     <div class="flex-1 h-px bg-black"></div>
                 </div>
             </div>
