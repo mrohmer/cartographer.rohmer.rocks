0cb1f783d2713f665ee38c82c0427f2440379af4
mrohmer <rohmer.matze@gmail.com>
2022-07-13 18:10:28 +0200

    feat: add some meta tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,6 +11,13 @@
 </script>
 <script lang="ts">
     import '../app.css';
+    import {_} from "svelte-i18n";
 </script>
 
+<svelte:head>
+    <title>{$_('meta.title', {default: 'Kartograph Scorekarte'})}</title>
+    <meta name="description" content="{$_('meta.description', {default: ''})}">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="theme-color" content="#000000" />
+</svelte:head>
 <slot />
