b2e20c89e84871a022c06f23bcc2bba6209ff5f0
mrohmer <rohmer.matze@gmail.com>
2022-07-15 21:14:08 +0200

    fix: fix previous games with empty maps

--- src/routes/_/components/MiniMap.svelte
+++ src/routes/_/components/MiniMap.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import GameField from "$lib/components/GameField.svelte";
   import type {GameMap} from '$lib/models/game-map';
+  import {createGameMap} from "$lib/utils/create-game-map.js";
 
   export let map: GameMap;
 </script>
 
 <div class="w-full h-full">
     <div class="w-[1000%] h-[1000%] origin-top-left" style="transform: scale(0.1)">
-        <GameField {map} showIcons={false} />
+        <GameField map={map ?? createGameMap('normal')} showIcons={false} />
     </div>
     <div class="absolute inset-0 w-full h-full"></div>
 </div>
