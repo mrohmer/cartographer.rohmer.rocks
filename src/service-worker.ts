953080ab18898216e8e65d9abdab31b1c7d4e277
mrohmer <rohmer.matze@gmail.com>
2022-07-18 18:27:39 +0200

    fix: fix offline usage

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,15 +1,38 @@
 import { build, files } from '$service-worker';
-import {registerRoute} from 'workbox-routing';
+import {setDefaultHandler, setCatchHandler} from 'workbox-routing';
 import {NetworkFirst} from 'workbox-strategies';
+import {warmStrategyCache} from 'workbox-recipes';
+
+
+const FALLBACK_GAME_URL = '/0/scorecard';
+
+const staticRoutesToCache = [
+	'/',
+	'/new',
+	'/new/wasteland',
+]
 
 const urls = [
-	'/',
+	FALLBACK_GAME_URL,
+	...staticRoutesToCache,
 	...build,
 	...files,
 ];
-registerRoute(
-	({url}) => urls.includes(url.pathname),
-	new NetworkFirst(),
-);
+
+const strategy = new NetworkFirst();
+warmStrategyCache({
+	urls,
+	strategy,
+});
+
+setDefaultHandler(strategy);
+
+setCatchHandler(async ({request, event}) => {
+	request.destination === 'document' && console.log(/\/\d+\/scorecard(\?|#|$)/.test(request.url))
+	if (request.destination === 'document' && /\/\d+(\?|#|$)/.test(request.url)) {
+		return strategy.handle({event, request: FALLBACK_GAME_URL});
+	}
+	return Response.error();
+})
 
 skipWaiting();
