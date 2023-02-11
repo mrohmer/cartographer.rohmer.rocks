45a4582bb26fd2262ef17b817b04f0c0a0757d61
mrohmer <rohmer.matze@gmail.com>
2023-02-11 20:57:42 +0100

    fix: fix cartographers error page

--- src/routes/cartographer/+error.svelte
+++ src/routes/cartographer/+error.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import {page} from "$app/stores";
-    import Error from "$lib/components/Error.svelte";
+    import Error from "./_/components/Error.svelte";
 </script>
 
 <Error status={$page.status}/>
